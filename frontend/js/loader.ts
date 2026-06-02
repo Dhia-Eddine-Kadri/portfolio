85420f209e4d48e1b245268298385fb45f964a7d
Dalimovich <dalimovich2004@gmail.com>
2026-06-02 18:37:23 +0200

    Make landing preview dialog opaque

--- frontend/js/loader.ts
+++ frontend/js/loader.ts
@@ -268,7 +268,7 @@ interface LandingTranslation {
         link.href = href;
         document.head.appendChild(link);
       }
-      ensureStylesheet('css/new-landing.css?v=4');
+      ensureStylesheet('css/new-landing.css?v=5');
       ensureStylesheet('css/auth.css?v=4');
     })();
 
